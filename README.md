b73a0cb4256c828e245bd28e44c6f4086d670623
Alex Fuchs <github@xathon.de>
2020-10-06 03:16:59 +0200

    add readme and database structure

---
+++ README.md
@@ -0,0 +1,18 @@
+#Logo Contest WIP
+If you host this yourself, make sure to create a .env file with the following contents:
+```
+DB_HOST="YOUR_HOST"
+DB_PASS="YOUR_PASSWORD"
+DB_USER="YOUR_DATABASE"
+DB_NAME="logocontest"
+DB_PORT="YOUR_PORT"
+```
+Also install the dependencies via `composer install`.
+
+##Usage
+Staff can upload logos via the upload.php site. On the main page, there's a link to the voting page and to the dynamic results page.
+There, the logos are ranked by their percentage of won matchups.
+
+##To-Do
+* Build a non-ugly frontend
+* Alter collected data as needed
---
+++ database.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS `logos`;
+CREATE TABLE `logos` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(64) NOT NULL,
+                         `matchups` int(11) NOT NULL DEFAULT 0,
+                         `won_matchups` int(11) NOT NULL DEFAULT 0,
+                         `logo` mediumblob NOT NULL,
+                         `region` varchar(2) NOT NULL DEFAULT 'EU',
+                         `division` varchar(15) NOT NULL DEFAULT 'Unlimited',
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `logos_name_uindex` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `matchups`;
+CREATE TABLE `matchups` (
+                            `id` varchar(20) NOT NULL,
+                            `winner` int(11) NOT NULL,
+                            `loser` int(11) NOT NULL,
+                            `user` varchar(30) NOT NULL,
+                            `time` datetime NOT NULL DEFAULT current_timestamp(),
+                            PRIMARY KEY (`id`),
+                            KEY `matchups_logos_id_fk` (`winner`),
+                            KEY `matchups___fk2` (`loser`),
+                            CONSTRAINT `matchups___fk` FOREIGN KEY (`winner`) REFERENCES `logos` (`id`) ON DELETE CASCADE,
+                            CONSTRAINT `matchups___fk2` FOREIGN KEY (`loser`) REFERENCES `logos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `output`;
+DROP VIEW IF EXISTS `output`;
+create view output as
+select *, (won_matchups/matchups) as won_ratio from logos order by won_ratio DESC;
